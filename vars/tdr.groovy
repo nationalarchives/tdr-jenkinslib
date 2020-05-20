71a95a8d3a3d82fc5f774c03b4292db66a8bc683
Lholbourn <loki.holbourn@nationalarchives.gov.uk>
2020-05-20 10:46:39 +0100

    Make functions more descriptive
    This change makes groovy functions more readable for the client. It
    removes certain parameters that are not needed.

--- vars/tdr.groovy
+++ vars/tdr.groovy
@@ -1,11 +1,37 @@
-def buildEndToEndTests(int delay, String buildJob, String[] params) {
-    build(job: buildJob, parameters: params, quietPeriod: delay, wait: false)
+// Call when deployment (intg or staging) has finished. Use the delay to ensure that the AWS load balancer allows access to the new version you are deploying.
+def runEndToEndTests(int delaySeconds, String stage) {
+    String[] params = [
+            string(name: "STAGE", value: stage)
+    ]
+    build(job: "TDRAcceptanceTest", parameters: params, quietPeriod: delaySeconds, wait: false)
 }
 
-def setBuildStatus(String status, String desc) {
+
+//It is important for TDR devs to know that the code they want to merge doesn't break TDR. By sending the build status for every commit (all branches) to GitHub we can ensure code that breaks TDR cannot be merged.
+
+// Call this when build starts (to let person who made changes know they are being checked) - call within first 'stage' of Jenkins pipeline actions.
+def reportStartOfBuildToGitHub() {
     withCredentials([string(credentialsId: 'github-jenkins-api-key', variable: 'GITHUB_ACCESS_TOKEN')]) {
         String sha = sh(script: 'git rev-parse HEAD', returnStdout: true).trim()
-        String url = "https://api.github.com/repos/nationalarchives/tdr-transfer-frontend/statuses/${sha}"
-        sh "curl -XPOST '${url}' -H 'Authorization: bearer ${env.GITHUB_ACCESS_TOKEN}' --data '{\"state\":\"${status}\",\"target_url\":\"${env.BUILD_URL}\",\"description\":\"${desc}\",\"context\":\"Jenkins TDR Front end test\"}'"
+        String url = "https://api.github.com/repos/nationalarchives/${repo}/statuses/${sha}"
+        sh "curl -XPOST '${url}' -H 'Authorization: bearer ${env.GITHUB_ACCESS_TOKEN}' --data '{\"state\":\"pending\",\"target_url\":\"${env.BUILD_URL}\",\"description\":\"Jenkins build has started\",\"context\":\"TDR Jenkins build status\"}'"
+    }
+}
+
+// Call when build finishes successfully - in Jenkins pipeline 'post' actions
+def reportSuccessfulBuildToGitHub() {
+    withCredentials([string(credentialsId: 'github-jenkins-api-key', variable: 'GITHUB_ACCESS_TOKEN')]) {
+        String sha = sh(script: 'git rev-parse HEAD', returnStdout: true).trim()
+        String url = "https://api.github.com/repos/nationalarchives/${repo}/statuses/${sha}"
+        sh "curl -XPOST '${url}' -H 'Authorization: bearer ${env.GITHUB_ACCESS_TOKEN}' --data '{\"state\":\"success\",\"target_url\":\"${env.BUILD_URL}\",\"description\":\"Jenkins build has completed successfully\",\"context\":\"TDR Jenkins build status\"}'"
+    }
+}
+
+// Call when build fails - in Jenkins pipeline 'post' actions
+def reportFailedBuildToGitHub() {
+    withCredentials([string(credentialsId: 'github-jenkins-api-key', variable: 'GITHUB_ACCESS_TOKEN')]) {
+        String sha = sh(script: 'git rev-parse HEAD', returnStdout: true).trim()
+        String url = "https://api.github.com/repos/nationalarchives/${repo}/statuses/${sha}"
+        sh "curl -XPOST '${url}' -H 'Authorization: bearer ${env.GITHUB_ACCESS_TOKEN}' --data '{\"state\":\"failure\",\"target_url\":\"${env.BUILD_URL}\",\"description\":\"Jenkins build has failed\",\"context\":\"TDR Jenkins build status\"}'"
     }
 }
