d402c5eb4baa453e23357c083cec8f5fa7fe914a
TomJKing <tom.king@nationalarchives.gov.uk>
2020-06-11 12:09:48 +0100

    Change name of params map
    Params map renamed to config to avoid confusion with client Jenkins job params
    
    Any parameters set in the client Jenkins job through the "Parameter" section are unavailable in the function and have to be passed into the function

--- vars/ecsDeployJob.groovy
+++ vars/ecsDeployJob.groovy
@@ -1,71 +1,71 @@
-def call(Map params) { 
+def call(Map config) {
   pipeline {
     agent {
       label "master"
     }
-      parameters {
-        choice(name: "STAGE", choices: ["intg", "staging", "prod"], description: "The stage you are deploying to")
-        string(name: "TO_DEPLOY", description: "The git tag, branch or commit reference to deploy, e.g. 'v123'")
-      }
-      stages {
-        stage("Docker") {
-          agent {
-            label "master"
-          }
-          steps {
-            script {
-              docker.withRegistry('', 'docker') {
-                sh "docker pull nationalarchives/${params.IMAGE_NAME}:${params.TO_DEPLOY}"
-                sh "docker tag nationalarchives/${params.IMAGE_NAME}:${params.TO_DEPLOY} nationalarchives/${params.IMAGE_NAME}:${params.STAGE}"
-                sh "docker push nationalarchives/${params.IMAGE_NAME}:${params.STAGE}"
-
-                slackSend color: "good", message: "*${params.IMAGE_NAME}* :whale: The '${params.TO_DEPLOY}' image has been tagged with '${params.STAGE}' in Docker Hub", channel: "#tdr-releases"
-              }
-            }
-          }
+    parameters {
+      choice(name: "STAGE", choices: ["intg", "staging", "prod"], description: "The stage you are deploying to")
+      string(name: "TO_DEPLOY", description: "The git tag, branch or commit reference to deploy, e.g. 'v123'")
+    }
+    stages {
+      stage("Docker") {
+        agent {
+          label "master"
         }
-        stage("Update ECS container") {
-          agent {
-            ecs {
-              inheritFrom "aws"
-              taskrole "arn:aws:iam::${env.MANAGEMENT_ACCOUNT}:role/TDRJenkinsNodeRole${params.STAGE.capitalize()}"
-            }
-          }
-          steps {
-            script {
-              def accountNumber = tdr.getAccountNumberFromStage("${params.STAGE}")
-
-              sh "python3 /update_service.py ${accountNumber} ${params.STAGE} ${params.ECS_SERVICE}"
-              slackSend color: "good", message: "*${params.ECS_SERVICE}* :arrow_up: The app has been updated in ECS in the *${params.STAGE}* environment", channel: "#tdr-releases"
-            }
-          }
-        }
-        stage("Update release branch") {
-          agent {
-            label "master"
-          }
-          steps {
-            script {
-              def releaseBranch = "release-${env.STAGE}"
-
-              sh "git branch -f ${releaseBranch} HEAD"
-              sshagent(['github-jenkins']) {
-                sh("git push -f origin ${releaseBranch}")
-              }
-            }
-          }
-        }
-      }
-      post {
-        success {
+        steps {
           script {
-            if (params.STAGE == "intg") {
-              int delaySeconds = params.TEST_DELAY_SECONDS
-                        
-              tdr.runEndToEndTests(delaySeconds, params.STAGE, BUILD_URL)
+            docker.withRegistry('', 'docker') {
+              sh "docker pull nationalarchives/${config.IMAGE_NAME}:${config.TO_DEPLOY}"
+              sh "docker tag nationalarchives/${config.IMAGE_NAME}:${config.TO_DEPLOY} nationalarchives/${config.IMAGE_NAME}:${config.STAGE}"
+              sh "docker push nationalarchives/${config.IMAGE_NAME}:${config.STAGE}"
+
+              slackSend color: "good", message: "*${config.IMAGE_NAME}* :whale: The '${config.TO_DEPLOY}' image has been tagged with '${config.STAGE}' in Docker Hub", channel: "#tdr-releases"
+            }
+          }
+        }
+      }
+      stage("Update ECS container") {
+        agent {
+          ecs {
+            inheritFrom "aws"
+            taskrole "arn:aws:iam::${env.MANAGEMENT_ACCOUNT}:role/TDRJenkinsNodeRole${config.STAGE.capitalize()}"
+          }
+        }
+        steps {
+          script {
+            def accountNumber = tdr.getAccountNumberFromStage("${config.STAGE}")
+
+            sh "python3 /update_service.py ${accountNumber} ${config.STAGE} ${config.ECS_SERVICE}"
+            slackSend color: "good", message: "*${config.ECS_SERVICE}* :arrow_up: The app has been updated in ECS in the *${cofnig.STAGE}* environment", channel: "#tdr-releases"
+          }
+        }
+      }
+      stage("Update release branch") {
+        agent {
+          label "master"
+        }
+        steps {
+          script {
+            def releaseBranch = "release-${env.STAGE}"
+
+            sh "git branch -f ${releaseBranch} HEAD"
+            sshagent(['github-jenkins']) {
+              sh("git push -f origin ${releaseBranch}")
             }
           }
         }
       }
     }
+    post {
+      success {
+        script {
+          if (config.STAGE == "intg") {
+            int delaySeconds = config.TEST_DELAY_SECONDS
+
+            tdr.runEndToEndTests(delaySeconds, config.STAGE, BUILD_URL)
+          }
+        }
+      }
+    }
+  }
 }
