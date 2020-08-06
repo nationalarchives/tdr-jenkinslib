aceaf621ae294368a3a99e1ff07170b4b748fdf8
sam <github@sampalmer.dev>
2020-08-06 12:31:29 +0100

    Add register provider in method

--- vars/tdr.groovy
+++ vars/tdr.groovy
@@ -15,6 +15,7 @@ def runEndToEndTests(int delaySeconds, String stage, String buildUrl) {
 
 def runGitSecrets(String repo) {
   sh "git-secrets --install -f"
+  sh "git-secrets --register-aws"
   sh "git-secrets --scan"
 }
 
