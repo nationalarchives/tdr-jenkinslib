2aaf22d4a9af96f8a16f75cc759c730c5295445a
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-10-13 13:54:41 +0100

    Use filename to label the uploaded artifact

--- vars/tdr.groovy
+++ vars/tdr.groovy
@@ -53,9 +53,10 @@ def assembleAndStash(String libraryName) {
 }
 
 def uploadReleaseArtifact(String path, String repo, String versionTag) {
+  String fileName = path.split("/").last()
   withCredentials([string(credentialsId: 'github-jenkins-api-key', variable: 'GITHUB_ACCESS_TOKEN')]) {
     String releaseId = sh(script: "curl -XPOST 'https://api.github.com/repos/nationalarchives/${repo}/releases'" +' -H "Authorization: bearer $GITHUB_ACCESS_TOKEN" ' + "-d '{\"tag_name\":\"${versionTag}\"}' | jq -r '.id'", returnStdout: true).trim()
-    sh("curl -XPOST --data-binary \"@${path}\" 'https://uploads.github.com/repos/nationalarchives/${repo}/releases/${releaseId}/assets?name=${repo}&label=${versionTag}'" +' -H "Authorization: bearer $GITHUB_ACCESS_TOKEN" ' + " -H 'Content-Type: application/octet-stream'")
+    sh("curl -XPOST --data-binary \"@${path}\" 'https://uploads.github.com/repos/nationalarchives/${repo}/releases/${releaseId}/assets?name=${fileName}&label=${versionTag}'" +' -H "Authorization: bearer $GITHUB_ACCESS_TOKEN" ' + " -H 'Content-Type: application/octet-stream'")
   }
 }
 
