c0e10de120de87ebfbffbe91ccd0b33c47c7e532
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-10-13 13:31:05 +0100

    Construct the upload url properly

--- vars/tdr.groovy
+++ vars/tdr.groovy
@@ -54,8 +54,8 @@ def assembleAndStash(String libraryName) {
 
 def uploadReleaseArtifact(String path, String repo, String versionTag) {
   withCredentials([string(credentialsId: 'github-jenkins-api-key', variable: 'GITHUB_ACCESS_TOKEN')]) {
-    String uploadUrl = sh(script: "curl -XPOST 'https://api.github.com/repos/nationalarchives/${repo}/releases'" +' -H "Authorization: bearer $GITHUB_ACCESS_TOKEN" ' + "-d '{\"tag_name\":\"${versionTag}\"}' | jq -r '.upload_url'", returnStdout: true).trim()
-    sh("curl -XPOST --data-binary \"@${path}\" '${uploadUrl}'" +' -H "Authorization: bearer $GITHUB_ACCESS_TOKEN" ' + " -H 'Content-Type: application/octet-stream'")
+    String releaseId = sh(script: "curl -XPOST 'https://api.github.com/repos/nationalarchives/${repo}/releases'" +' -H "Authorization: bearer $GITHUB_ACCESS_TOKEN" ' + "-d '{\"tag_name\":\"${versionTag}\"}' | jq -r '.id'", returnStdout: true).trim()
+    sh("curl -XPOST --data-binary \"@${path}\" 'https://uploads.github.com/repos/nationalarchives/${repo}/releases/${releaseId}/assets?name=${repo}&label=${versionTag}'" +' -H "Authorization: bearer $GITHUB_ACCESS_TOKEN" ' + " -H 'Content-Type: application/octet-stream'")
   }
 }
 
